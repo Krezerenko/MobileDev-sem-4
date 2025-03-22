88bae61d04774ce7cf019af293be54e7fcdc3720
Krezon <krezony@gmail.com>
2025-03-22 08:02:28 +0300

    Заполнил файлы разметки
    Также начал писать код

--- Projects/Pract4/app/src/main/java/com/example/pract4/MainActivity.java
+++ Projects/Pract4/app/src/main/java/com/example/pract4/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.pract4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +26,18 @@ public class MainActivity extends AppCompatActivity
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText editFirstName = findViewById(R.id.edit_main_firstName);
+        EditText editLastName = findViewById(R.id.edit_main_lastName);
+        Button buttonNext = findViewById(R.id.button_main_next);
+
+        buttonNext.setOnClickListener(v ->
+        {
+            Intent intent = new Intent(v.getContext(), SecondActivity.class);
+            intent.putExtra("ARGUMENT_FIRST_NAME", editFirstName.getText().toString());
+            intent.putExtra("ARGUMENT_LAST_NAME", editLastName.getText().toString());
+            startActivity(intent);
         });
     }
 }
