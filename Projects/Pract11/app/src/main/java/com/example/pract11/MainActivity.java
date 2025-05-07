a9e61f632699dc8f860f8a8a67a96851863145b5
Krezon <krezony@gmail.com>
2025-05-07 15:41:58 +0300

    Создал проект для 11 практики, а также выполнил и описал первый ее пункт

---
+++ Projects/Pract11/app/src/main/java/com/example/pract11/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.pract11;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
+        {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        WebView webView = findViewById(R.id.web_main);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://online-edu.mirea.ru/mod/assign/view.php?id=716754");
+    }
+}
