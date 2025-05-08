799df086c1d364c4010c9e284e0c55f8b412d0a3
Krezon <krezony@gmail.com>
2025-05-08 18:13:57 +0300

    Реализовал запись json файла, создал проект для чтения json файла

--- Projects/Pract12/app/src/main/java/com/example/pract12/MainActivity.java
+++ Projects/Pract12/app/src/main/java/com/example/pract12/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.pract12;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.gson.Gson;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -24,6 +34,30 @@ public class MainActivity extends AppCompatActivity
             return insets;
         });
 
+        EditText editName = findViewById(R.id.edit_main_name);
+        EditText editSurname = findViewById(R.id.edit_main_surname);
+        EditText editGroup = findViewById(R.id.edit_main_group);
+        EditText editAge = findViewById(R.id.edit_main_age);
 
+        Button buttonWrite = findViewById(R.id.button_main_write);
+        buttonWrite.setOnClickListener(v ->
+        {
+            Gson parser = new Gson();
+            Student student = new Student(
+                    editName.getText().toString(),
+                    editSurname.getText().toString(),
+                    editGroup.getText().toString(),
+                    Integer.parseInt(editAge.getText().toString()));
+            try (FileOutputStream out = openFileOutput("student", MODE_PRIVATE))
+            {
+                String json = parser.toJson(student);
+                out.write(json.getBytes(StandardCharsets.UTF_8));
+            }
+            catch (IOException e)
+            {
+                Toast.makeText(this, "Не удалось записать данные в файл",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
